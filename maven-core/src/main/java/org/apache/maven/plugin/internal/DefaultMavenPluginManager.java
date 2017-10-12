34f8ce9149b62ab10c413b2ece30e4a32324608a
Michael Simacek <msimacek@redhat.com>
2017-10-12 14:55:46 +0300

    [MNG-6294] Convert MavenPluginValidator into a plexus component

--- maven-core/src/main/java/org/apache/maven/plugin/internal/DefaultMavenPluginManager.java
+++ maven-core/src/main/java/org/apache/maven/plugin/internal/DefaultMavenPluginManager.java
@@ -163,6 +163,9 @@ public class DefaultMavenPluginManager
     @Requirement
     private PluginArtifactsCache pluginArtifactsCache;
 
+    @Requirement
+    private MavenPluginValidator pluginValidator;
+
     private ExtensionDescriptorBuilder extensionDescriptorBuilder = new ExtensionDescriptorBuilder();
 
     private PluginDescriptorBuilder builder = new PluginDescriptorBuilder();
@@ -240,14 +243,13 @@ public class DefaultMavenPluginManager
             throw new PluginDescriptorParsingException( plugin, pluginFile.getAbsolutePath(), e );
         }
 
-        MavenPluginValidator validator = new MavenPluginValidator( pluginArtifact );
+        List<String> errors = new ArrayList<>();
+        pluginValidator.validate( pluginArtifact, pluginDescriptor, errors );
 
-        validator.validate( pluginDescriptor );
-
-        if ( validator.hasErrors() )
+        if ( !errors.isEmpty() )
         {
             throw new InvalidPluginDescriptorException(
-                "Invalid plugin descriptor for " + plugin.getId() + " (" + pluginFile + ")", validator.getErrors() );
+                "Invalid plugin descriptor for " + plugin.getId() + " (" + pluginFile + ")", errors );
         }
 
         pluginDescriptor.setPluginArtifact( pluginArtifact );
